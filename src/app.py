ff084d2f53d4d995603841b690d1b0a333510a4a
jim-sheldon <jimisasheldon@gmail.com>
2022-05-20 23:14:14 -0400

    Added Docker, dependency management, build pipeline

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
-from datetime import date
+from datetime import datetime
 import json
+import logging
 from os import environ
+import sys
 
 import boto3
 import pygsheets
@@ -10,12 +12,17 @@ S3_BUCKET = environ.get("S3_BUCKET")
 
 DOCUMENT_ID = environ.get("DOCUMENT_ID")
 
-# FIXME: env var
-CREDS_FILE = "credentials.json"
+
+def setup_logger():
+    h = logging.StreamHandler(sys.stdout)
+    rootLogger = logging.getLogger()
+    rootLogger.addHandler(h)
+    rootLogger.setLevel(logging.DEBUG)
 
 
 def get_data():
-	client = pygsheets.authorize(service_file=CREDS_FILE)
+	logging.info("Getting data from Google Sheets")
+	client = pygsheets.authorize(service_account_env_var="GOOGLE_CREDENTIALS")
 	spreadsheet = client.open_by_key(DOCUMENT_ID)
 
 	data = spreadsheet[0].get_all_records()
@@ -24,12 +31,14 @@ def get_data():
 
 
 def clean_data(data):
+	logging.info("Cleaning data")
 	for point in data:
 		point.pop("Curator_initials")
 	return data
 
 
 def format_data(data):
+	logging.info("Formatting data")
 	json_data = json.dumps(data)
 	csv_data = ""
 	column_names = data[0].keys()
@@ -44,10 +53,10 @@ def format_data(data):
 
 
 def store_data(json_data, csv_data):
-
+	logging.info("Uploading data to S3")
 	s3 = boto3.resource("s3")
 
-	now = date.today()
+	now = datetime.today()
 
 	file_name = f"{now}.json"
 
@@ -61,7 +70,10 @@ def store_data(json_data, csv_data):
 
 
 if __name__ == "__main__":
+	setup_logger()
+	logging.info("Starting script")
 	data = get_data()
 	data = clean_data(data)
 	json_data, csv_data = format_data(data)
 	store_data(json_data, csv_data)
+	logging.info("Script completed")
