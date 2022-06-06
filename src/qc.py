3aa5dde259bcb510524895a46170326d3d152f06
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-06-06 16:01:01 +0100

    Add pandas and pyyaml to dependencies for QC

--- src/qc.py
+++ src/qc.py
@@ -2,17 +2,19 @@
 Lint monkeypox data at https://github.com/globaldothealth/monkeypox
 
 Raises errors and warnings to a Slack channel which should have
-a defined webhook at WEBHOOK_URL
+a defined webhook at LINT_WEBHOOK_URL
 
 Can also be run from the command line to show a list of errors and warnings
 """
 
+import os
 import sys
 import math
 import yaml
 import datetime
 import logging
 
+import requests
 import pandas as pd
 
 
@@ -43,7 +45,7 @@ def valid_int(s):
 
 
 def valid_enum(s, values):
-    return s.lower() in [v.lower() for v in values]
+    return str(s).strip().lower() in [v.lower() for v in values]
 
 
 def valid_url(s):
@@ -55,12 +57,16 @@ def valid_url(s):
 
 
 def valid_integer_range(value):
+    if isinstance(value, float) or isinstance(value, int):
+        return False  # not a range
     if "<" in value:
         return valid_int(value.replace("<", ""))
     if ">" in value:
         return valid_int(value.replace(">", ""))
     nums = value.split("-")
-    return len(nums) == 2 and valid_int(x := nums[0]) and valid_int(y := nums[1]) and x < y
+    return (
+        len(nums) == 2 and valid_int(x := nums[0]) and valid_int(y := nums[1]) and x < y
+    )
 
 
 def is_empty(value):
@@ -71,8 +77,8 @@ def is_empty(value):
     return False
 
 
-def validate_field(value, field_name, field_type, optional=True):
-    if optional and is_empty(value):
+def validate_field(value, field_name, field_type, required=False):
+    if not required and is_empty(value):
         return True
     if "|" in field_type:
         return valid_enum(value, field_type.split(" | "))
@@ -85,23 +91,28 @@ def validate_field(value, field_name, field_type, optional=True):
     elif field_type == "integer-range":
         return valid_integer_range(value)
     elif field_name == "Country_ISO3":
-        return value.isupper() and len(value) == 3
+        return isinstance(value, str) and value.isupper() and len(value) == 3
     else:
         return True
 
 
 def lint(file_or_url):
     linting_result = []
+    line = 0
     for row in pd.read_csv(file_or_url).to_dict("records"):
+        line += 1
         invalid_fields = [
             (field_name, value)
             for field_name, value in row.items()
-            if not validate_field(value, field_name, types[field_name])
+            if not validate_field(
+                value, field_name, types[field_name], required[field_name]
+            )
         ]
         if invalid_fields:
             linting_result.append(
                 {
                     "id": row["ID"],
+                    "line": line,
                     "errors": [
                         {"field": field_name, "value": value}
                         for field_name, value in invalid_fields
@@ -113,12 +124,29 @@ def lint(file_or_url):
 
 
 def pretty_lint_results(results, header=""):
-    return header + "\n" + "\n".join(
-        f"* *{row['id']}*: "
-        + ", ".join(f"{e['field']}={e['value']}" for e in row["errors"])
-        for row in results
-    )
+    if results:
+        return (
+            header
+            + "\n"
+            + "\n".join(
+                f"* *{row['id']}* (line {row['line']}): "
+                + ", ".join(f"{e['field']}={e['value']}" for e in row["errors"])
+                for row in results
+            )
+        )
+
+
+def send_slack_message(webhook_url: str, message: str) -> None:
+    if (response := requests.post(webhook_url, json={"text": message})).status != 200:
+        logging.error(
+            f"Slack notification failed with {response.status_code}: {response.text}"
+        )
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    print(pretty_lint_results(lint(sys.argv[1]), header=f"Linting {sys.argv[1]}:"))
+    results = pretty_lint_results(lint(sys.argv[1]), header=f"Linting {sys.argv[1]}:")
+    if results:
+        print(results)
+    if results and (webhook_url := os.getenv("LINT_WEBHOOK_URL")):
+        send_slack_message(webhook_url, results)
