3d474e0133df6b801c25936d9d94a7e472fb274d
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-06-06 16:02:50 +0100

    Change module name

--- src/test_qc.py
+++ src/test_qc.py
@@ -1,25 +1,25 @@
 import pytest
 
-import lint
+import qc
 
 
 @pytest.mark.parametrize(
     "source,expected", [("2020-01-01", True), ("", True), ("hello", False)]
 )
 def test_valid_date(source, expected):
-    assert lint.valid_date(source) == expected
+    assert qc.valid_date(source) == expected
 
 
 @pytest.mark.parametrize("source,expected", [("1", True), ("hello", False)])
 def test_valid_int(source, expected):
-    assert lint.valid_int(source) == expected
+    assert qc.valid_int(source) == expected
 
 
 @pytest.mark.parametrize(
     "source,expected", [("Female", True), ("male", True), ("unknown", False)]
 )
 def test_valid_enum(source, expected):
-    assert lint.valid_enum(source, ["male", "female", "other"]) == expected
+    assert qc.valid_enum(source, ["male", "female", "other"]) == expected
 
 
 @pytest.mark.parametrize(
@@ -31,18 +31,18 @@ def test_valid_enum(source, expected):
     ],
 )
 def test_valid_url(source, expected):
-    assert lint.valid_url(source) == expected
+    assert qc.valid_url(source) == expected
 
 
 @pytest.mark.parametrize(
     "source,expected", [("<40", True), (">5", True), ("20-25", True), ("50-10", False)]
 )
 def test_valid_integer_range(source, expected):
-    assert lint.valid_integer_range(source) == expected
+    assert qc.valid_integer_range(source) == expected
 
 
 @pytest.mark.parametrize(
     "source,expected", [("", True), (float("nan"), True), ("hello", False)]
 )
 def test_is_empty(source, expected):
-    assert lint.is_empty(source) == expected
+    assert qc.is_empty(source) == expected
