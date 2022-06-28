41449f928eb76005ecf00bfa3860c9de44386b91
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-06-28 19:58:36 +0100

    timeseries: fill series upto today with zeros

--- src/test_timeseries.py
+++ src/test_timeseries.py
@@ -5,6 +5,8 @@ from pandas import Timestamp
 
 import timeseries
 
+TODAY = pd.Timestamp(2022, 6, 9)
+
 DATA = pd.read_csv(
     io.StringIO(
         """
@@ -30,6 +32,10 @@ BY_CONFIRMED = [
     {"Date": Timestamp("2022-06-03 00:00:00"), "Cases": 3, "Cumulative_cases": 5},
     {"Date": Timestamp("2022-06-04 00:00:00"), "Cases": 0, "Cumulative_cases": 5},
     {"Date": Timestamp("2022-06-05 00:00:00"), "Cases": 4, "Cumulative_cases": 9},
+    {"Date": Timestamp("2022-06-06 00:00:00"), "Cases": 0, "Cumulative_cases": 9},
+    {"Date": Timestamp("2022-06-07 00:00:00"), "Cases": 0, "Cumulative_cases": 9},
+    {"Date": Timestamp("2022-06-08 00:00:00"), "Cases": 0, "Cumulative_cases": 9},
+    {"Date": Timestamp("2022-06-09 00:00:00"), "Cases": 0, "Cumulative_cases": 9},
 ]
 
 BY_COUNTRY_CONFIRMED = [
@@ -48,6 +54,42 @@ BY_COUNTRY_CONFIRMED = [
     {
         "Date": Timestamp("2022-06-03 00:00:00"),
         "Cases": 3,
+        "Cumulative_cases": 4,
+        "Country": "England",
+    },
+    {
+        "Date": Timestamp("2022-06-04 00:00:00"),
+        "Cases": 0,
+        "Cumulative_cases": 4,
+        "Country": "England",
+    },
+     {
+        "Date": Timestamp("2022-06-05 00:00:00"),
+        "Cases": 0,
+        "Cumulative_cases": 4,
+        "Country": "England",
+    },
+     {
+        "Date": Timestamp("2022-06-06 00:00:00"),
+        "Cases": 0,
+        "Cumulative_cases": 4,
+        "Country": "England",
+    },
+     {
+        "Date": Timestamp("2022-06-07 00:00:00"),
+        "Cases": 0,
+        "Cumulative_cases": 4,
+        "Country": "England",
+    },
+     {
+        "Date": Timestamp("2022-06-08 00:00:00"),
+        "Cases": 0,
+        "Cumulative_cases": 4,
+        "Country": "England",
+    },
+     {
+        "Date": Timestamp("2022-06-09 00:00:00"),
+        "Cases": 0,
         "Cumulative_cases": 4,
         "Country": "England",
     },
@@ -75,14 +117,40 @@ BY_COUNTRY_CONFIRMED = [
         "Cumulative_cases": 5,
         "Country": "USA",
     },
+    {
+        "Date": Timestamp("2022-06-06 00:00:00"),
+        "Cases": 0,
+        "Cumulative_cases": 5,
+        "Country": "USA",
+    },
+    {
+        "Date": Timestamp("2022-06-07 00:00:00"),
+        "Cases": 0,
+        "Cumulative_cases": 5,
+        "Country": "USA",
+    },
+    {
+        "Date": Timestamp("2022-06-08 00:00:00"),
+        "Cases": 0,
+        "Cumulative_cases": 5,
+        "Country": "USA",
+    },
+    {
+        "Date": Timestamp("2022-06-09 00:00:00"),
+        "Cases": 0,
+        "Cumulative_cases": 5,
+        "Country": "USA",
+    },
+
 ]
 
 
 def test_by_confirmed():
-    assert timeseries.by_confirmed(DATA).to_dict("records") == BY_CONFIRMED
+    assert timeseries.by_confirmed(DATA, TODAY).to_dict("records") == BY_CONFIRMED
 
 
 def test_by_country_confirmed():
     assert (
-        timeseries.by_country_confirmed(DATA).to_dict("records") == BY_COUNTRY_CONFIRMED
+        timeseries.by_country_confirmed(DATA, TODAY).to_dict("records")
+        == BY_COUNTRY_CONFIRMED
     )
